625ed7f4a6983a11d7ffaa88c9c033ca59f24258
prasanna.mahajan <mahajanprasannap@gmail.com>
2018-05-11 18:00:13 +0530

    adding code

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "success",
+			args: args{
+				a: 3,
+				b: 4,
+			},
+			want: 7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Sum(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Sum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
